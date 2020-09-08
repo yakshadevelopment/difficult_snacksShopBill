6da92850fcd35d7c4fd4a16c431050be670e7325
Administrator <Administrator@sumedha-PC>
2020-09-08 16:07:54 +0530

    Updated and uploaded

--- app1/src/test/java/java9/diff/app1/MainTest.java
+++ app1/src/test/java/java9/diff/app1/MainTest.java
@@ -10,4 +10,15 @@ public class MainTest {
 		String value=Main.salesTaxCalculation(9315.00);
 		yakshaAssert(currentTest(),(value.equals("886.12")?"true":"false"),businessTestFile);
 	}
+	@Test
+    public void testExceptionConditon() throws Exception{
+	 yakshaAssert(currentTest(),true,boundaryTestFile);
+      }
+
+	@Test
+	public void testBoundaryCondition() throws Exception {
+	  yakshaAssert(currentTest(),true,exceptionTestFile);
+   }
+
+
 }
