f9028ed965bda25ad5d8f161e8b606805ebdb179
Administrator <Administrator@sumedha-PC>
2020-09-10 17:48:48 +0530

    updated and uploded

--- app1/src/main/java/java9/diff/app1/ItemData.java
+++ app1/src/main/java/java9/diff/app1/ItemData.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Stream;
 
--- app1/src/main/java/java9/diff/app1/OrderItem.java
+++ app1/src/main/java/java9/diff/app1/OrderItem.java
@@ -1,12 +1,6 @@
 package java9.diff.app1;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrderItem {
 	private String itemName;
 	private String rate;
@@ -14,12 +8,93 @@ public class OrderItem {
 	private String actualAmount;
 	private String discount;
 	private String payAmount;
+	
 	public OrderItem(String itemName, String rate, String orderQty) {
 		super();
 		this.itemName = itemName;
 		this.rate = rate;
 		this.orderQty = orderQty;
 	}
+	
+	
+	public OrderItem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public OrderItem(String itemName, String rate, String orderQty, String actualAmount, String discount,
+			String payAmount) {
+		super();
+		this.itemName = itemName;
+		this.rate = rate;
+		this.orderQty = orderQty;
+		this.actualAmount = actualAmount;
+		this.discount = discount;
+		this.payAmount = payAmount;
+	}
+   
+
+	public String getItemName() {
+		return itemName;
+	}
+
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+
+	public String getRate() {
+		return rate;
+	}
+
+
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+
+
+	public String getOrderQty() {
+		return orderQty;
+	}
+
+
+	public void setOrderQty(String orderQty) {
+		this.orderQty = orderQty;
+	}
+
+
+	public String getActualAmount() {
+		return actualAmount;
+	}
+
+
+	public void setActualAmount(String actualAmount) {
+		this.actualAmount = actualAmount;
+	}
+
+
+	public String getDiscount() {
+		return discount;
+	}
+
+
+	public void setDiscount(String discount) {
+		this.discount = discount;
+	}
+
+
+	public String getPayAmount() {
+		return payAmount;
+	}
+
+
+	public void setPayAmount(String payAmount) {
+		this.payAmount = payAmount;
+	}
+
+
 	@Override
 	public String toString() {
 			String output=String.format("%-15s %-10s %-10s %-10s %-10s %-10s ",itemName,rate,orderQty,actualAmount,discount,payAmount);
